1cd160de3e59010f6bdfb586321ea5cda5129d77
Marshall Beddoe <unmarshal@gmail.com>
2011-02-20 12:49:13 -0800

    shuffle cards

--- lib/texas/deck.rb
+++ lib/texas/deck.rb
@@ -1,15 +1,38 @@
 class Deck
+  attr_reader :deck
 
   def initialize
-    @cards = []
+    @deck = [
+      'Ac' , 'Ah' , 'Ad' , 'As',
+      'Kc' , 'Kh' , 'Kd' , 'Ks',
+      'Qc' , 'Qh' , 'Qd' , 'Qs',
+      'Jc' , 'Jh' , 'Jd' , 'Js',
+      '10c', '10h', '10d', '10s',
+      '9c' , '9h' , '9d' , '9s',
+      '8c' , '8h' , '8d' , '8s',
+      '7c' , '7h' , '7d' , '7s',
+      '6c' , '6h' , '6d' , '6s',
+      '5c' , '5h' , '5d' , '5s',
+      '4c' , '4h' , '4d' , '4s',
+      '3c' , '3h' , '3d' , '3s',
+      '2c' , '2h' , '2d' , '2s',
+    ]
   end
 
   def shuffle
-    # shuffle the deck
+    7.times do
+      (@deck.size-1).downto(1) do |i|
+        ri = rand(i+1) # ruby's rand good enough? doubt it
+        card = @deck[i]
+        @deck[i] = @deck[ri]
+        @deck[ri] = card
+      end
+    end
   end
 
   def deal
-    # pop card off stack
+    card = @deck.shift
+    @deck.push card
+    return card
   end
-
 end
---
+++ lib/texas/hand.rb
@@ -0,0 +1,11 @@
+class Hand
+
+  # collection of 5 cards in any order
+
+  # need to find type of hand
+
+  # need logic to rank hands
+
+  # handle kicker cards, etc
+
+end
